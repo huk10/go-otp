99abcd235357c0765ee027cbd7808dfbf8a77d1d
Hu Kun <mrhukun@gmail.com>
2024-01-12 00:08:26 +0800

    :clapper: Add demo

---
+++ example/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/huk10/go-otp"
+	"os"
+	"time"
+)
+
+func main() {
+	// 随机生成一个 20 字节的秘钥
+	secret := otp.Base32Encode(otp.RandomSecret(20))
+	totp := otp.NewTOTP(secret)
+	// 基于当前时间生成一个 token
+	token := totp.Now()
+	// 校验 token 是否在指定的时间有效
+	if totp.Verify(token, time.Now()) {
+		// token 有效
+	}
+	// 生成一个二维码，此可二维码可以使用 Google Authenticator 扫码导入。
+	png, err := totp.KeyURI("alice@google.com", "Example").QRCode()
+	if err != nil {
+		panic(err)
+	}
+	// 将二维码写入到本地文件中
+	err = os.WriteFile("./example/qrcode.png", png, 0666)
+	if err != nil {
+		panic(err)
+	}
+}
